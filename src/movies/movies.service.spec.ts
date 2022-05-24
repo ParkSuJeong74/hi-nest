f3d39f046c3f73cc114225d29f4685a893ebf60e
ParkSuJeong <sujeong9158@gmail.com>
2022-05-24 22:59:48 +0900

    feat: e2e testing

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -13,7 +13,17 @@ describe('MoviesService', () => {
     }).compile();
 
     service = module.get<MoviesService>(MoviesService);
+
+    service.create({   // movie test용 생성
+        title:"Test Movie",
+        genres: ["Test"],
+        year: 2000,
+      })
   });
+
+  afterAll(() => {
+
+  })
 
   it('should be defined', () => {
     expect(service).toBeDefined();
@@ -28,25 +38,71 @@ describe('MoviesService', () => {
   
   describe("getOne", () => {
     it("should return a movie", () => {
-      service.create({   // movie test용 생성
-        title:"Test Movie",
-        genres: ["Test"],
-        year: 2000,
-      })
-
       const movie = service.getOne(1)
       expect(movie).toBeDefined()
       expect(movie.id).toEqual(1)
     })
     
-    it("shoule throw 404 error", () => {
+    it("should throw 404 error", () => {
       try{
         service.getOne(999)
       } catch(e) {
         expect(e).toBeInstanceOf(NotFoundException)
-        expect(e.message).toEqual(`Not Found Movie with ID : 999`)
+        expect(e.message).toEqual(`Not Found Movie with ID : 999`) // 보통 안씀!
+      }
+    })
+  })
+  describe("deleteOne", () => {
+    it("delete a movie", () => {
+      const beforeDelete = service.getAll().length
+      service.deleteOne(1)
+      const afterDelete = service.getAll().length
+
+      expect(afterDelete).toBeLessThan(beforeDelete) // toEqual(beforeDelete - 1)
+    })
+
+    it("should throw 404 error", () => {
+      try{
+        service.deleteOne(999)
+      } catch(e) {
+        expect(e).toBeInstanceOf(NotFoundException)
       }
     })
   })
 
+  describe("create", () => {
+    it("should create a movie", () => {
+      const beforeCreate = service.getAll().length
+      service.create({   // movie test용 생성
+        title:"Test Movie",
+        genres: ["Test"],
+        year: 2000,
+      })
+      
+      const afterCreate = service.getAll().length
+      expect(afterCreate).toBeGreaterThan(beforeCreate) // toEqual(beforeDelete - 1)
+    })
+  })
+
+  describe("update", () => {
+    it("should update a movie", () => {
+      service.create({   // movie test용 생성
+        title:"Test Movie",
+        genres: ["Test"],
+        year: 2000,
+      })
+      
+      service.update(1, { title: 'updated test'})
+      const movie = service.getOne(1)
+      expect(movie.title).toEqual('updated test')
+    })
+
+    it("should throw a NotFoundException", () => {
+      try{
+        service.update(999, {})
+      } catch(e) {
+        expect(e).toBeInstanceOf(NotFoundException)
+      }
+    })
+  })
 });
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    // 테스팅 환경에서도 필요
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true, // 어떤 decorator도 없는 property의 object는 거름
+        forbidNonWhitelisted: true,// 이상한걸 보내면 request를 막음
+        transform: true, // 원하는 타입으로 변환
+      })
+    )
     await app.init();
   });
 
@@ -19,6 +27,72 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my Movie API');
   });
+
+  describe('/movies', () => {
+
+    it('GET', () => {
+      return request(app.getHttpServer()) // 서버
+        .get('/movies')
+        .expect(200)
+        .expect([]); // 지금은 빈 DB 사용
+    });
+
+    it('POST 201', () => {
+      return request(app.getHttpServer()) // 서버
+        .post('/movies')
+        .send({
+          title:'Test',
+          year:2000,
+          genres: ['test']
+        })
+        .expect(201)
+    });
+
+    it('POST 400', () => {
+      return request(app.getHttpServer()) // 서버
+        .post('/movies')
+        .send({
+          title:'Test',
+          year:2000,
+          genres: ['test'],
+          other:"thing"
+        })
+        .expect(400)
+    });
+
+    // 없기 때문에 404
+    it('DELETE', () => {
+      return request(app.getHttpServer())
+      .delete('/movies')
+      .expect(404)
+    })
+  })
+
+  describe('/movies/id', () => {
+    it.todo('GET') // 만들어야할 때
+    
+    it('GET 200', () => {
+      return request(app.getHttpServer())
+      .get('/movies/1') // id는 number type, but string(transform 필요)
+      .expect(200)
+    })
+    it('GET 400', () => {
+      return request(app.getHttpServer())
+      .get('/movies/999') // id는 number type, but string(transform 필요)
+      .expect(404)
+    })
+    it('PATCH', () => {
+      return request(app.getHttpServer())
+      .patch('/movies/1')
+      .send({title:'updated test'})
+      .expect(200)
+    })
+    it('DELETE', () => {
+      return request(app.getHttpServer())
+      .delete('/movies/1')
+      .expect(200)
+    })
+  })
 });
